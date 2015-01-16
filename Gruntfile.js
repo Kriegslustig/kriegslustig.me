1a109a9022a5d0b72e5c0c0f8923dfd1f406bfef
Kriegslustig <github@ls7.ch>
2015-01-16 18:44:14 +0100

    Adding jshint and clean

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,32 @@
 module.exports = function (grunt) {
-  grunt.initConfig({
+  grunt
+  .initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {
       dist: {
         options: {
-          style: "expanded"
-        },
-        files: {
-          "public/css/styles.css": "sass/styles.scss"
+          style: 'expanded'
+        }
+        , files: {
+          'public/css/styles.css': 'sass/styles.scss'
         }
       }
     }
+    , clean: [
+      '.sass-cache'
+    ]
+    , jshint: {
+      options: {
+        'asi': true
+        , 'laxcomma': true
+      }
+      , all: ['Gruntfile.js', 'js/**/*.js'],
+    }
   })
-
   grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-clean')
+  grunt.loadNpmTasks('grunt-contrib-jshint')
 
-  grunt.registerTask('default', ['sass'])
+  grunt.registerTask('default', ['sass', 'jshint'])
+  grunt.registerTask('live', ['clean'])
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kriegslustig.me
 My humble portfolio
+
+## Requirements
+Requires [node.js](http://nodejs.org/) and [grunt](http://gruntjs.com/getting-started) to install.
+
+## Installation
+`npm install`
