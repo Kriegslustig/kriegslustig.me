96472efe00223606fc12613cb50787a999a73f7b
Kriegslustig <github@ls7.ch>
2015-01-20 22:38:46 +0100

    Adding the /blog/post/title route

--- client/router.js
+++ client/router.js
@@ -1,16 +1,28 @@
 Router.route('/', function () {
-  this.render('pageFront')
+  var self = this
+  self.render('pageFront')
 })
 
 Router.route('/blog', function () {
   var self = this
-  var queryHash = this.params.query
-  var limit = parseInt(queryHash.limit)
-  var skip = parseInt(queryHash.skip)
+  var queryDict = self.params.query
+  var limit = parseInt(queryDict.limit)
+  var skip = parseInt(queryDict.skip)
   Meteor.call('getPosts', limit, skip, function (err, posts) {
     if(!err) {
       posts = EJSON.parse(posts)
-      self.render('pageBlog', {data: { posts: posts}})
+      self.render('pageBlog', {data: {posts: posts}})
+    }
+  })
+})
+
+Router.route('/blog/post/:title', function () {
+  var self = this
+  var postUrlTitle = self.params.title
+  Meteor.call('getSinglePost', postUrlTitle, function (err, post) {
+    if(!err) {
+      post = EJSON.parse(post)
+      self.render('pagePost', {data: post})
     }
   })
 })
--- server/methods.js
+++ server/methods.js
@@ -1,13 +1,23 @@
 Meteor.methods({
   getPosts: function (limit, skip) {
+    var posts = []
     skip = skip || 0
     limit = limit || 10
+    orion.entities.posts.collection.find({}, {
+      sort: {createdAt: -1}
+    , limit: limit
+    , skip: skip
+    }).forEach(function (post) {
+      post.link = '/blog/post/' + encodeURIComponent(post.title)
+      posts.push(post)
+    })
+
+    return EJSON.stringify(posts)
+  }
+, getSinglePost: function (urlTitle) {
+    var title = decodeURIComponent(urlTitle)
     return EJSON.stringify(
-      orion.entities.posts.collection.find({}, {
-        sort: {createdAt: -1}
-      , limit: limit
-      , skip: skip
-      }).fetch()
+      orion.entities.posts.collection.findOne({title: title})
     )
   }
 })
