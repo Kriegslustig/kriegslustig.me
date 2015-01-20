6b83b441cec52688d5c7079d4ae1caa700459962
Kriegslustig <github@ls7.ch>
2015-01-20 20:13:01 +0100

    Removing a console.log, adding some styles for small and wide screens

--- client/router.js
+++ client/router.js
@@ -6,7 +6,6 @@ Router.route('/blog', function () {
   var self = this
   Meteor.call('getPosts', function (err, posts) {
     if(!err) {
-      console.log(posts)
       posts = EJSON.parse(posts)
       self.render('pageBlog', {data: { posts: posts}})
     }
