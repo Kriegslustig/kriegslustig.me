989973e56743c9f7edc03a6b6bffc97fae3545cf
Kriegslustig <github@ls7.ch>
2015-01-26 20:06:21 +0100

    Name the routes and use the iron pathFor helper

--- client/routes.js
+++ client/routes.js
@@ -1,17 +1,26 @@
-Router.route('/', function () {
-  var self = this
-  self.render('pageFront')
+Router.route('/', {
+  name: 'front'
+, action: function () {
+    var self = this
+    self.render('pageFront')
+  }
 })
 
-Router.route('/journal', function () {
-  var self = this
-  orion.subs.subscribe('entity', 'posts')
-  self.render('pageJournal')
+Router.route('/journal', {
+  name: 'journal'
+, action: function () {
+    var self = this
+    orion.subs.subscribe('entity', 'posts')
+    self.render('pageJournal')
+  }
 })
 
-Router.route('/journal/post/:title', function () {
-  var self = this
-, postUrlTitle = self.params.title
-, post = getSinglePostByTitle(postUrlTitle)
-  self.render('pagePost', {data: post})
+Router.route('/journal/post/:title', {
+  name: 'journal.post'
+, action: function () {
+    var self = this
+  , postUrlTitle = self.params.title
+  , post = getSinglePostByTitle(postUrlTitle)
+    self.render('pagePost', {data: post})
+  }
 })
