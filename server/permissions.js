2e09950b2e01069c7adef3a4e7c2bc098df7e995
Kriegslustig <github@ls7.ch>
2015-05-07 00:05:09 +0200

    Add basic permissions

---
+++ server/permissions.js
@@ -0,0 +1,11 @@
+Journal_entries.allow({
+  insert: function () {
+    return !!Meteor.userId()
+  },
+  update: function () {
+    return !!Meteor.userId()
+  },
+  remove: function () {
+    return !!Meteor.userId()
+  }
+})
