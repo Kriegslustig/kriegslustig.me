30a0a01115d8fe1bef1a583e712da1f02d6b5345
Kriegslustig <github@ls7.ch>
2015-05-06 21:53:18 +0200

    Add a journal_entry route

--- client/router.js
+++ client/router.js
@@ -5,3 +5,11 @@ Router.route('/', {
 Router.route('/about', {
   name: 'about',
 })
+
+Router.route('/journal/:title', {
+  name: 'journal_entry',
+  template: 'journal_entry',
+  data: function () {
+    return Journal_entries.findOne({title: this.params.title})
+  }
+})
