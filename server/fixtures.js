74bc0ff05c6e31b110c4b197ee97d39850a306b0
Kriegslustig <github@ls7.ch>
2015-05-06 21:36:37 +0200

    Fix a bug in the fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -2,7 +2,8 @@ if(Journal_entries.find().count() == 0) {
   Journal_entries.insert({
     title: 'Test Something',
     md_body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-  }, {
+  })
+  Journal_entries.insert({
     title: 'In other news',
     md_body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   })
