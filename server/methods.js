284ea1b16bf2e379496ae98c72e7c5dcd04eb8d9
Kriegslustig <github@ls7.ch>
2015-05-06 20:45:40 +0200

    Clean slate!

--- server/methods.js
+++
@@ -1,8 +0,0 @@
-Meteor.methods({
-  getPosts: function (limit, skip) {
-    return getPosts(limit, skip)
-  }
-, getSinglePostByTitle: function (title) {
-    return getSinglePostByTitle(title)
-  }
-})
--- server/routes.js
+++
@@ -1,3 +0,0 @@
-Router.route('/journal/json', function () {
-  this.response.end(EJSON.stringify(orion.entities.posts.collection.find({}).fetch()))
-}, {where: 'server'})
