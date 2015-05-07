368dcb4b5d27b2bbd1579eb4fe10217f9906988f
Kriegslustig <github@ls7.ch>
2015-05-07 19:06:32 +0200

    Fix a bug in the insert form

--- client/helpers/new_journal_entry.js
+++ client/helpers/new_journal_entry.js
@@ -5,10 +5,8 @@ Template.new_journal_entry.events({
     e.preventDefault()
     console.log(Journal_entries.findOne(wrappingForm.getAttribute('data-id')))
     Journal_entries.insert({
-      $set: {
         title: wrappingForm.querySelector('input[type=text]').value,
         md_body: wrappingForm.querySelector('textarea').value
-      }  
     }, function (err, _id) {
       if(_id) Router.go('journal')
     })
