804fd6f2e4e5f906f5928e885f39f4e18fd1301a
Kriegslustig <github@ls7.ch>
2015-05-08 14:05:51 +0200

    Add red_flash

--- client/lib/utils.js
+++ client/lib/utils.js
@@ -1,10 +1,18 @@
-green_flash = function () {
+function flash (someColor) {
   var someElement = document.createElement('div')
   someElement.className = 'green_flash'
   document.body.appendChild(someElement)
-  someElement.style.background = "hsl(120, 80%, 90%)"
+  someElement.style.background = someColor
   setTimeout(function () {
     someElement.style.background = ""
     document.body.removeChild(someElement)
   }, 60)
+}
+
+green_flash = function () {
+  flash("hsl(120, 80%, 90%)")
+}
+
+red_flash = function () {
+  flash("hsl(10, 80%, 90%)")
 }
