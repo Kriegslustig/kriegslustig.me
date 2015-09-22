0748e46778bdfd3ca7ca5e8502b41c92d20bff6a
Kriegslustig <github@ls7.ch>
2015-09-22 20:54:56 +0200

    Fix the JSON export

--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,10 @@
-IsoRouter.route('/json/journal')
-  .action(function (params) {
-    this.res.end(JSON.stringify(JournalEntries.find({published: true}).fetch()))
-  })
 
-IsoRouter.route('/json/journal/:title')
-  .action(function (params) {
-    this.res.end(JSON.stringify(JournalEntries.findOne({title: params.title})))
-  })
+var route = IsoRouter.route('/json/journal')
+  .action(Meteor.bindEnvironment(function (params) {
+      route.res.end(JSON.stringify(JournalEntries.find({published: true}).fetch()))
+  }), () => {}, global.route)
+
+var routeSingle = IsoRouter.route('/json/journal/:title')
+  .action(Meteor.bindEnvironment(function (params) {
+      routeSingle.res.end(JSON.stringify(JournalEntries.findOne({title: decodeURIComponent(params.title)})))
+  }), () => {}, global.routeSingle)
