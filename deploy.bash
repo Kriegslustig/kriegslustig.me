d71b386632afb00f6e9a1cb5102d55928f058fd1
Kriegslustig <github@ls7.ch>
2015-09-22 21:24:53 +0200

    Move the app to a higher port

--- deploy.bash
+++ deploy.bash
@@ -9,4 +9,4 @@ cp settings.json .demeteorized/ && \
 docker build -t ${project_name} . && \
 docker stop ${project_name} && \
 docker rm ${project_name} && \
-docker run -dp 8282:80 --link mongo_${project_name}:mongo --name ${project_name} --restart=always ${project_name}
+docker run -dp 8383:80 --link mongo_${project_name}:mongo --name ${project_name} --restart=always ${project_name}
