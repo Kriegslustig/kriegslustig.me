d670af7e0b176350db509810e268d742dcd59753
Kriegslustig <github@ls7.ch>
2015-09-22 21:09:44 +0200

    Add a deploy script

---
+++ deploy.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+project_name=kriegslustig_me
+
+cd /var/docker/kriegslustig.me && \
+git pull && \
+demeteorizer && \
+cp settings.json .demeteorized/ && \
+docker build -t ${project_name} . && \
+docker stop ${project_name} && \
+docker rm ${project_name} && \
+docker run -dp 8282:80 --link mongo_${project_name}:mongo --name ${project_name} --restart=always ${project_name}
